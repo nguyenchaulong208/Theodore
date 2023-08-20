eec650a062211ccb2a8fd8c54f129ee8b0668edc
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-08-20 18:04:29 +0700

    Update

---
+++ Text_To_Speech_Source/Text_To_Speech_Source/Text_To_Speech_Source/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace Text_To_Speech_Source
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnResume = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.btnResume);
+            this.groupBox1.Controls.Add(this.txtText);
+            this.groupBox1.Controls.Add(this.btnPause);
+            this.groupBox1.Controls.Add(this.btnPlay);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(7, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(792, 444);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // btnResume
+            // 
+            this.btnResume.Location = new System.Drawing.Point(265, 336);
+            this.btnResume.Name = "btnResume";
+            this.btnResume.Size = new System.Drawing.Size(94, 35);
+            this.btnResume.TabIndex = 5;
+            this.btnResume.Text = "Resume";
+            this.btnResume.UseVisualStyleBackColor = true;
+            this.btnResume.Click += new System.EventHandler(this.btnResume_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(32, 81);
+            this.txtText.Multiline = true;
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(731, 233);
+            this.txtText.TabIndex = 4;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(144, 336);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(94, 35);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(32, 336);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(94, 35);
+            this.btnPlay.TabIndex = 2;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Text";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(407, 336);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Button btnResume;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Text_To_Speech_Source/Text_To_Speech_Source/Text_To_Speech_Source/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Speech.AudioFormat;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Text_To_Speech_Source
+{
+
+    public partial class Form1 : Form
+    {
+       
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+
+        }
+        SpeechSynthesizer reader = new SpeechSynthesizer();
+        
+            
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            //foreach (InstalledVoice voice in reader.GetInstalledVoices())
+            //{
+            //VoiceInfo info = voice.VoiceInfo;
+            //string AudioFormats = "";
+            //foreach (SpeechAudioFormatInfo fmt in info.SupportedAudioFormats)
+            //{
+            //    AudioFormats += String.Format("{0}\n",
+            //    fmt.EncodingFormat.ToString());
+            //}
+            //reader.SelectVoiceByHints(VoiceGender.Female);
+
+            foreach (var voice in reader.GetInstalledVoices(new CultureInfo("vi-VN")))
+            {
+                var info = voice.VoiceInfo;
+                reader.SelectVoice("Microsoft An Desktop");
+
+            }
+
+            reader.SetOutputToDefaultAudioDevice();
+
+
+
+            if (txtText.Text != "")
+            {
+                reader.SpeakAsync(txtText.Text);
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter text!", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+
+        }
+       
+
+private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (reader != null)
+            {
+                
+                    reader.Pause();
+                
+
+            }    
+        }
+
+        private void btnResume_Click(object sender, EventArgs e)
+        {
+            if(reader != null)
+                reader.Resume();
+        }
+    }
+}
