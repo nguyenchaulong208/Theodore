171490f88f8e02f7837ca72e825587451704a518
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-08-21 00:33:02 +0700

    Update

---
+++ TRAINNING/C#/109_MinValueUsingParams/109_MinValueUsingParams/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _109_MinValueUsingParams
+{
+   
+    public class Program
+    {
+        public static int MinValue(params int[] numbers)
+        {
+            int min = int.MaxValue;
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+            }
+            return min;
+        }
+        static void Main(string[] args)
+        {
+            double sum1 = MinValue(0,-11,1, 2754, 3);
+            Console.WriteLine($"Ket qua cua sum1: {sum1}");
+
+        }
+    }
+}
---
+++ TRAINNING/C#/UDEMY/108_Why would we use Params/108_Why would we use Params/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _108_Why_would_we_use_Params
+{
+    
+    public class Program
+    {
+        public static double Sum(params int[] numbers)
+        {
+            int count = 0;
+            int total = 0;
+            foreach (int number in numbers)
+            {
+                total += number;
+                count++;
+            }
+            return (double) total/count;
+        }
+    
+        static void Main(string[] args)
+        {
+            //khoi tao cac bien va goi ham Sum voi cac parameter khong xac dinh duoc so luong cu the.
+            double sum1 = Sum(1,2,3);
+            double sum2 = Sum(2,3,4,5);
+            double sum3 = Sum();
+            Console.WriteLine($"Ket qua cua sum1: {sum1}");
+            Console.WriteLine($"Ket qua cua sum1: {sum2}");
+            Console.WriteLine($"Ket qua cua sum1: {sum3}");
+            
+            
+        }
+    }
+}
