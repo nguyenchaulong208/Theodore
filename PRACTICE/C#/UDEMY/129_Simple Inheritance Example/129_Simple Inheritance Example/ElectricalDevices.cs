b58204ff5b96f11f0b1c060184acc96716b70522
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-08-21 04:21:27 +0700

    Update

---
+++ PRACTICE/C#/UDEMY/129_Simple Inheritance Example/129_Simple Inheritance Example/ElectricalDevices.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _129_Simple_Inheritance_Example
+{
+    public class ElectricalDevices
+    {
+        //boolean to determin the state of the Electrical Devices
+        public bool IsOn { get; set; }
+        //string for the branch name of the Electrical Device
+        public string Brand { get; set; }
+        //Contructor
+        public ElectricalDevices(bool power, string brandName)
+        {
+            this.IsOn = power;
+            this.Brand = brandName;
+        }
+
+        public void SwitchOn()
+        {
+            IsOn = true;
+        }
+        public void SwitchOff()
+        {
+            IsOn = false;
+        }
+    }
+}
