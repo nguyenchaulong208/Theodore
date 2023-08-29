394853c6b67c3a13a0660cca60a9a5cbb2bc64f8
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-08-30 02:30:10 +0700

    Update

---
+++ PRACTICE/C#/EXERCISE/Ex1/Ex1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1: Calculate S(n) = 1 + 2 + 3 + … + n
+            Console.WriteLine("1: Calculate S(n) = 1 + 2 + 3 + … + n");
+            Console.WriteLine("-----");
+            int n = 0, result = 0;
+            Console.Write("Please input n: ");
+            n = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= n; i++)
+            {
+                result = result + i;
+            }  
+            Console.WriteLine($"S = {result}");
+        }
+    }
+}
