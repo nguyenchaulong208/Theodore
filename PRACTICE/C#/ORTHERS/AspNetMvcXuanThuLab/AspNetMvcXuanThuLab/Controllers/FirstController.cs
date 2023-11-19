8e84a475fb1f314d7018ca5126900fdee25a95c8
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2023-11-19 23:29:52 +0700

    Update

---
+++ PRACTICE/C#/ORTHERS/AspNetMvcXuanThuLab/AspNetMvcXuanThuLab/Controllers/FirstController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace AspNetMvcXuanThuLab.Controllers
+{
+    public class FirstController : Controller
+    {
+        public string test()
+        {
+            return "Day la First index";
+        }
+        //GET: First
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        
+    }
+}
