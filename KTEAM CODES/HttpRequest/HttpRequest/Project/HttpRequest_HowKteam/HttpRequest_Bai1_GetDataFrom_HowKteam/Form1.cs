0bccb95d5232d0fae1bc2997edc31536cc93c4e7
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-02-28 02:25:11 +0700

    update
    HttpRequest code

--- KTEAM CODES/HttpRequest/HttpRequest/Project/HttpRequest_HowKteam/HttpRequest_Bai1_GetDataFrom_HowKteam/Form1.cs
+++ KTEAM CODES/HttpRequest/HttpRequest/Project/HttpRequest_HowKteam/HttpRequest_Bai1_GetDataFrom_HowKteam/Form1.cs
@@ -30,20 +30,21 @@ namespace HttpRequest_Bai1_GetDataFrom_HowKteam
              HttpWebRequest
              HttpRequest
              */
-
-            var html = GetData("https://www.howkteam.com/");
-            TestData(html);
+            //Lấy dữ liệu từ trang web HowKteam: soucre code của trang web
+            var html = GetData("https://www.howkteam.com/"); //GetData là hàm tự định nghĩa
+            TestData(html);//TestData là hàm tự định nghĩa
         }
 
         void TestData(string html)
         {
-            File.WriteAllText("res.html", html);
-            Process.Start("res.html");
+            File.WriteAllText("res.html", html);//Lưu source code của trang web vào file res.html
+            Process.Start("res.html");//Mở file res.html
         }
 
-        void AddCookie(HttpRequest http, string cookie)
+        void AddCookie(HttpRequest http, string cookie)//Thêm cookie vào HttpRequest
         {
-            var temp = cookie.Split(';');
+            var temp = cookie.Split(';');//Tách chuỗi cookie thành các phần tử con
+            //Lặp qua từng phần tử con và thêm vào HttpRequest dưới dạng key-value
             foreach (var item in temp)
             {
                 var temp2 = item.Split('=');
@@ -54,20 +55,21 @@ namespace HttpRequest_Bai1_GetDataFrom_HowKteam
             }
         }
 
-        string GetData(string url, string cookie = null)
+        string GetData(string url, string cookie = null)//Lấy dữ liệu trang web
         {
-            HttpRequest http = new HttpRequest();
-            http.Cookies = new CookieDictionary();
+            HttpRequest http = new HttpRequest();//Khởi tạo HttpRequest để request về trang web HowKteam
+            http.Cookies = new CookieDictionary();//Khởi tạo CookieDictionary để lưu cookie của trang web sau khi request về thành công
 
             
-            if (!string.IsNullOrEmpty(cookie))
+            if (!string.IsNullOrEmpty(cookie))//Nếu cookie không rỗng thì thêm cookie vào HttpRequest để request về trang web
             {
                 AddCookie(http, cookie);
             }
-
+            //Thiết lập UserAgent để request về trang web
             http.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36";
-            string html = http.Get(url).ToString();
-            return html;
+            
+            string html = http.Get(url).ToString();//Request về trang web và lấy source code của trang web
+            return html;//Trả về source code của trang web
         }
 
         private void button2_Click(object sender, EventArgs e)
