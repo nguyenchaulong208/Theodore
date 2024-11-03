e8aae05330a2ae4fbcf8ebf600df4b5eeacf656c
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2024-11-04 03:53:21 +0700

    upload file

---
+++ HCMUS/Lap Trinh Ung Dung Quan Ly 2/Tuan_3 Example 1/Tuan_3 Example 1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tuan_3_Example_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void configButton(object sender, RoutedEventArgs e)
+        {
+            var screen = new ConfigWindow(); //Var sẽ tự nhận diện kiểu dữ liệu theo đối tượng bên trái
+            screen.Owner = this;
+            screen.Show();
+        }
+
+        private void logoutButton(object sender, RoutedEventArgs e)
+        {
+            var screen = new LoginWindow();
+            screen.Show();
+            this.Close();
+        }
+    }
+}
