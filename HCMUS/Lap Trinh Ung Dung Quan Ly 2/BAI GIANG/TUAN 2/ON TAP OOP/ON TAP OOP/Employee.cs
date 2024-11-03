845b335d324aca06a0fc9ef7a2b1d07883aedc31
Theodore <nguyenchaulong.data208@gmail.com>
2024-11-04 04:48:08 +0700

    up

---
+++ HCMUS/Lap Trinh Ung Dung Quan Ly 2/BAI GIANG/TUAN 2/ON TAP OOP/ON TAP OOP/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ON_TAP_OOP
+{
+    public class Employee
+    {
+        //Atributes
+        public string FirstName;
+        public string LastName;
+        public string Tel;
+    }
+}
