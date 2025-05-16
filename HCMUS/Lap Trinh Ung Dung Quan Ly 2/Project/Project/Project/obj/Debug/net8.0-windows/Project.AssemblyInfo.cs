29c0475e744bad151b5ddd3e4139ba3639000d87
Theodore <113950597+nguyenchaulong208@users.noreply.github.com>
2025-05-17 03:05:59 +0700

    update

--- HCMUS/Lap Trinh Ung Dung Quan Ly 2/Project/Project/Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
+++ HCMUS/Lap Trinh Ung Dung Quan Ly 2/Project/Project/Project/obj/Debug/net8.0-windows/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f719aae2ef1b4cebf28297e1eb683f4a7f4bda8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b68ddc33ef8cd94d0771b200bd43b4a5604423a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
